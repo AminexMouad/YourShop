f3f4abf878d35c396ee20b85ce51931b4402e219
Amine.mouad <amouad@umanis.com>
2021-01-29 17:18:09 +0100

    Environment Variables

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 
 const app = express();
 
@@ -13,8 +16,12 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/product/:id', (req, res) => {
   const product = products.find((product) => product._id == req.params.id);
-  console.log(product);
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
