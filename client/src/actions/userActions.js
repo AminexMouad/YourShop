1fff0d3bd03c8773d680e13900edf9100d39e3ab
Amine.mouad <amouad@umanis.com>
2021-02-02 16:33:06 +0100

    User Login Reducer & Action

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import {
+  LOGIN_USER_FAIL,
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_SUCCESS,
+} from '../constants/userConstants';
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: LOGIN_USER_REQUEST });
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    );
+
+    dispatch({ type: LOGIN_USER_SUCCESS, payload: data });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: LOGIN_USER_FAIL,
+      payload: error.response.data.message
+        ? error.response.data.message
+        : error.message,
+    });
+  }
+};
