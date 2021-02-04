57cf0374b8c668bd51b5483265f31610fd38fb8b
Amine.mouad <amouad@umanis.com>
2021-02-04 12:28:18 +0100

    Get Order by ID Endpoint

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
 // @desc  Create new order
-// @route  GET /api/orders
+// @route  POST /api/orders
 // @access  private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
@@ -35,4 +35,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder);
   }
 });
-export { addOrderItems };
+
+// @desc  Get order by id
+// @route  GET /api/orders
+// @access  private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+export { addOrderItems, getOrderById };
