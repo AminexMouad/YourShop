15735065f03127cb4a1b7c715ce978cd23290801
Amine.mouad <amouad@umanis.com>
2021-02-05 12:40:10 +0100

    Get User By ID & Update User Endpoints

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -80,7 +80,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc  Update the user profile
 // @route  PUT /api/users/profile
 // @access  Private
-const updaterUser = asyncHandler(async (req, res) => {
+const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
@@ -128,11 +128,51 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc  Get user By ID
+// @route  Get /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
+
+// @desc  Update yser
+// @route  PUT /api/users/:id
+// @access  Private
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 export {
   authUser,
   getUserProfile,
   registerUser,
-  updaterUser,
+  updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,16 +3,24 @@ const router = express.Router();
 import {
   authUser,
   deleteUser,
+  getUserById,
   getUserProfile,
   getUsers,
   registerUser,
-  updaterUser,
+  updateUser,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 
-router.route('/').get(protect, admin, getUsers);
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile).put(protect, updaterUser);
-router.route('/:id').delete(protect, admin, deleteUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 export default router;
