c79364bfb0862641c7e6f880f22da46c392c39ea
Amine.mouad <amouad@umanis.com>
2021-02-08 17:01:36 +0100

    Product Search

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -5,7 +5,9 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Product from '../components/Product';
 import { listProducts } from '../actions/productActions';
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword;
+
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
@@ -13,8 +15,8 @@ const HomeScreen = () => {
   const { loading, error, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   return (
     <>
