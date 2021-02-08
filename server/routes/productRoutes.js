774e289251869b5de14a2a9deb2125373cc7b95f
Amine.mouad <amouad@umanis.com>
2021-02-08 16:14:04 +0100

    Front End Product Reviews

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -11,7 +11,7 @@ import {
 import { admin, protect } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id/reviews').post(protect, admin, createProductReview);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 router
   .route('/:id')
