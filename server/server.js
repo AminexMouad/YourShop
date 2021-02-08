2b96697be195abe3ad611b9e85b907d0be3c9b0f
Amine.mouad <amouad@umanis.com>
2021-02-08 15:37:31 +0100

    Morgan & Create Review Endpoint

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import colors from 'colors';
+import morgan from 'morgan';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -15,6 +16,9 @@ connectDB();
 
 const app = express();
 
+if (process.env.NODE_ENV === 'developement') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.get('/', (req, res) => {
